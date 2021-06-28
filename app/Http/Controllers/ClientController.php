2e133f8e452d14f051fe7ae9ca490a040ac835da
stipolovrenovic <74571935+stipolovrenovic@users.noreply.github.com>
2021-06-28 20:15:46 +0200

    Dodane rute
    Dodane su get rute za svaki ekran, post ruta za stvaranje klijenta,  put ruta za ažuriranje klijenta i delete ruta za brisanje klijenta.

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,7 +14,7 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        return view()
     }
 
     /**
@@ -24,7 +24,7 @@ class ClientController extends Controller
      */
     public function create()
     {
-        //
+        return view('clients.create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clients', function () {
+    return view('clients.index', [
+    	'clients' => App\Client::all();
+    ]);
+});
+
+Route::get('/clients', 'ClientController@index');
+Route::post('/clients', 'ClientController@store');
+Route::get('/clients/create', 'ClientController@create');
+Route::get('/clients/{client}/edit', 'ClientController@edit');
+Route::put('/clients', 'ClientController@update');
+Route::delete('/clients', 'ClientController@delete');
