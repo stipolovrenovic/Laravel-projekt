780e2183ce2101a6a0adc66fcadad7bd68222f7e
stipolovrenovic <74571935+stipolovrenovic@users.noreply.github.com>
2021-06-28 20:14:29 +0200

    Dodani ekrani za klijente
    Dodani su ekrani za listu klijenata, te za stvaranje novog klijenta i njegovo uređivanje (trenutno nema sadržaja)

---
+++ resources/views/clients/edit.blade.php
@@ -0,0 +1,8 @@
+@extends ('layout')
+
+@section ('head')
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+@endsection
+
+@section ('content')
+@endsection
