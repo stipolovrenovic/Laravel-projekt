bc4abd3ebb14e0d42860ba3581573eeef2fa1fd2
stipolovrenovic <74571935+stipolovrenovic@users.noreply.github.com>
2021-07-02 13:39:16 +0200

    Dodane invalid poruke i maknut maxlength
    Dodane su invalid poruke za polja (trenutno jos nije implementirana validacija); maknut maxlength jer on ne funkcionira za input number

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -3,29 +3,45 @@
 <a class="btn btn-secondary" href="{{ route('clients.index') }}">Povratak na listu</a>
 <form action="{{ route('clients.store') }}" method="POST">
  @csrf
-  <div class="form-group">
+
+   <div class="form-group">
     <label for="inputName">Ime i prezime</label>
-    <input type="text" class="form-control" id="inputName" name="name" placeholder = "Unesite ime i prezime klijenta..." required="required">
+    <input type="text" class="form-control" id="inputName" name="name" required="required" aria-describedby="inputNameFeedback">
+    <div id="inputNameFeedback" class="invalid-feedback">
+      Niste unijeli ime.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputAddress">Adresa</label>
-    <input type="text" class="form-control" id="inputAddress" name="address" placeholder = "Unesite adresu prebivališta klijenta..." required="required">
+    <input type="text" class="form-control" id="inputAddress" name="address" required="required" aria-describedby="inputAddressFeedback">
+    <div id="inputAddressFeedback" class="invalid-feedback">
+      Niste unijeli adresu.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputPostcode">Poštanski broj</label>
-    <input type="number" class="form-control" id="inputPostcode" name="postcode" placeholder = "Unesite poštanski broj klijenta..." required="required" maxlength="5">
+    <input type="number" class="form-control" id="inputPostcode" name="postcode" required="required" aria-describedby="inputPostcodeFeedback">
+    <div id="inputPostcodeFeedback" class="invalid-feedback">
+      Niste unijeli poštanski broj.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputCity">Grad</label>
-    <input type="text" class="form-control" id="inputCity" name="city" placeholder = "Unesite grad..." required="required">
+    <input type="text" class="form-control" id="inputCity" name="city" aria-describedby="inputCityFeedback">
+    <div id="inputCityFeedback" class="invalid-feedback">
+      Niste unijeli grad.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputCountry">Država</label>
-    <input type="text" class="form-control" id="inputCountry" name="country" placeholder = "Unesite državu..." required="required">
+    <input type="text" class="form-control" id="inputCountry" name="country" required="required" aria-describedby="inputCountryFeedback">
+    <div id="inputCountryFeedback" class="invalid-feedback">
+      Niste unijeli državu.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputOIB">OIB</label>
-    <input type="number" class="form-control" id="inputOIB" name="oib" placeholder = "Unesite OIB klijenta..." maxlength="11">
+    <input type="number" class="form-control" id="inputOIB" name="oib">
   </div>
   <button type="submit" class="btn btn-primary">Spremi</button>
 </form>
--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -7,23 +7,38 @@
 
   <div class="form-group">
     <label for="inputName">Ime i prezime</label>
-    <input type="text" class="form-control" id="inputName" name="name" value="{{ $client->name }}" required="required">
+    <input type="text" class="form-control" id="inputName" name="name" value="{{ $client->name }}" required="required" aria-describedby="inputNameFeedback">
+    <div id="inputNameFeedback" class="invalid-feedback">
+      Niste unijeli ime.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputAddress">Adresa</label>
-    <input type="text" class="form-control" id="inputAddress" name="address" value="{{ $client->address }}" required="required">
+    <input type="text" class="form-control" id="inputAddress" name="address" value="{{ $client->address }}" required="required" aria-describedby="inputAddressFeedback">
+    <div id="inputAddressFeedback" class="invalid-feedback">
+      Niste unijeli adresu.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputPostcode">Poštanski broj</label>
-    <input type="number" class="form-control" id="inputPostcode" name="postcode" value="{{ $client->postcode }}" required="required" maxlength="5">
+    <input type="number" class="form-control" id="inputPostcode" name="postcode" value="{{ $client->postcode }}" required="required" maxlength="5" aria-describedby="inputPostcodeFeedback">
+    <div id="inputPostcodeFeedback" class="invalid-feedback">
+      Niste unijeli poštanski broj.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputCity">Grad</label>
-    <input type="text" class="form-control" id="inputCity" name="city" value="{{ $client->city }}">
+    <input type="text" class="form-control" id="inputCity" name="city" value="{{ $client->city }}" aria-describedby="inputCityFeedback">
+    <div id="inputCityFeedback" class="invalid-feedback">
+      Niste unijeli grad.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputCountry">Država</label>
-    <input type="text" class="form-control" id="inputCountry" name="country" value="{{ $client->country }}" required="required">
+    <input type="text" class="form-control" id="inputCountry" name="country" value="{{ $client->country }}" required="required" aria-describedby="inputCountryFeedback">
+    <div id="inputCountryFeedback" class="invalid-feedback">
+      Niste unijeli državu.
+    </div>
   </div>
   <div class="form-group">
     <label for="inputOIB">OIB</label>
