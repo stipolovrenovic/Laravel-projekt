6a24ae20afe0d5c9ea71a2aefe85e8a705c7a943
stipolovrenovic <74571935+stipolovrenovic@users.noreply.github.com>
2021-07-05 13:01:55 +0200

    Ažurirana sintaksa formi

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -4,72 +4,72 @@
 <form action="{{ route('clients.store') }}" method="POST">
  @csrf
 
-   <div class="form-group">
-    <label for="inputName">Ime i prezime</label>
-    <input type="text" class="form-control @if($errors->has('name')) is-invalid @endif" id="inputName" name="name" value="{{ old('name') }}" aria-describedby="inputNameFeedback">
-    
-    @if($errors->has('name'))
-    <div id="inputNameFeedback" class="invalid-feedback">
-      {{ $errors->first('name') }}
-    </div>
-    @endif
-
+ <div class="form-group">
+  <label for="inputName">Ime i prezime</label>
+  <input type="text" class="form-control @error('name') is-invalid @enderror" id="inputName" name="name" value="{{ old('name') }}" aria-describedby="inputNameFeedback">
+  
+  @error('name')
+  <div id="inputNameFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputAddress">Adresa</label>
-    <input type="text" class="form-control @if($errors->has('address')) is-invalid @endif" id="inputAddress" name="address" value="{{ old('address') }}" aria-describedby="inputAddressFeedback">
-   
-    @if($errors->has('address'))
-    <div id="inputAddressFeedback" class="invalid-feedback">
-      {{ $errors->first('address') }}
-    </div>
-    @endif
+  @enderror
 
+</div>
+<div class="form-group">
+  <label for="inputAddress">Adresa</label>
+  <input type="text" class="form-control @error('address') is-invalid @enderror" id="inputAddress" name="address" value="{{ old('address') }}" aria-describedby="inputAddressFeedback">
+  
+  @error('address')
+  <div id="inputAddressFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputPostcode">Poštanski broj</label>
-    <input type="number" class="form-control @if($errors->has('postcode')) is-invalid @endif" id="inputPostcode" name="postcode" value="{{ old('postcode') }}"  aria-describedby="inputPostcodeFeedback">
-   
-    @if($errors->has('postcode'))
-    <div id="inputPostcodeFeedback" class="invalid-feedback">
-      {{ $errors->first('postcode') }}
-    </div>
-    @endif
+  @enderror
 
+</div>
+<div class="form-group">
+  <label for="inputPostcode">Poštanski broj</label>
+  <input type="number" class="form-control @error('postcode') is-invalid @enderror" id="inputPostcode" name="postcode" value="{{ old('postcode') }}"  aria-describedby="inputPostcodeFeedback">
+  
+  @error('postcode')
+  <div id="inputPostcodeFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputCity">Grad</label>
-    <input type="text" class="form-control @if($errors->has('city')) is-invalid @endif" id="inputCity" name="city" value="{{ old('city') }}" aria-describedby="inputCityFeedback">
+  @enderror
 
-    @if($errors->has('city'))
-    <div id="inputCityFeedback" class="invalid-feedback">
-      {{ $errors->first('city') }}
-    </div>
-    @endif
+</div>
+<div class="form-group">
+  <label for="inputCity">Grad</label>
+  <input type="text" class="form-control @error('city') is-invalid @enderror" id="inputCity" name="city" value="{{ old('city') }}" aria-describedby="inputCityFeedback">
 
+  @error('city')
+  <div id="inputCityFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputCountry">Država</label>
-    <input type="text" class="form-control @if($errors->has('country')) is-invalid @endif" id="inputCountry" name="country" value="{{ old('country') }}" aria-describedby="inputCountryFeedback">
+  @enderror
 
-    @if($errors->has('country'))
-    <div id="inputCountryFeedback" class="invalid-feedback">
-      {{ $errors->first('country') }}
-    </div>
-    @endif
+</div>
+<div class="form-group">
+  <label for="inputCountry">Država</label>
+  <input type="text" class="form-control @error('country') is-invalid @enderror" id="inputCountry" name="country" value="{{ old('country') }}" aria-describedby="inputCountryFeedback">
 
+  @error('country')
+  <div id="inputCountryFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputOIB">OIB</label>
-    <input type="number" class="form-control @if($errors->has('oib')) is-invalid @endif" id="inputOIB" name="oib" value="{{ old('oib') }}"  aria-describedby="inputOIBFeedback">
+  @enderror
 
-    <!@if($errors->has('oib'))
-    <div id="inputOIBFeedback" class="invalid-feedback">
-      {{ $errors->first('oib') }}
-    </div>
-    @endif
+</div>
+<div class="form-group">
+  <label for="inputOIB">OIB</label>
+  <input type="number" class="form-control @error('oib') is-invalid @enderror" id="inputOIB" name="oib" value="{{ old('oib') }}"  aria-describedby="inputOIBFeedback">
 
+  @error('oib')
+  <div id="inputOIBFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <button type="submit" class="btn btn-primary">Spremi</button>
+  @enderror
+
+</div>
+<button type="submit" class="btn btn-primary">Spremi</button>
 </form>
 @endsection
--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -5,72 +5,72 @@
  @csrf
  @method('PUT')
 
-  <div class="form-group">
-    <label for="inputName">Ime i prezime</label>
-    <input type="text" class="form-control @if($errors->has('name')) is-invalid @endif" id="inputName" name="name" value="{{ $client->name }}" aria-describedby="inputNameFeedback">
-     
-    @if($errors->has('name'))
-    <div id="inputNameFeedback" class="invalid-feedback">
-      {{ $errors->first('name') }}
-    </div>
-    @endif
-
+ <div class="form-group">
+  <label for="inputName">Ime i prezime</label>
+  <input type="text" class="form-control @error('name') is-invalid @enderror" id="inputName" name="name" value="{{ $client->name }}" aria-describedby="inputNameFeedback">
+  
+  @error('name')
+  <div id="inputNameFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputAddress">Adresa</label>
-    <input type="text" class="form-control @if($errors->has('address')) is-invalid @endif" id="inputAddress" name="address" value="{{ $client->address }}" aria-describedby="inputAddressFeedback">
-     
-    @if($errors->has('address'))
-    <div id="inputAddressFeedback" class="invalid-feedback">
-      {{ $errors->first('address') }}
-    </div>
-    @endif
+  @enderror
 
+</div>
+<div class="form-group">
+  <label for="inputAddress">Adresa</label>
+  <input type="text" class="form-control @error('address') is-invalid @enderror" id="inputAddress" name="address" value="{{ $client->address }}" aria-describedby="inputAddressFeedback">
+  
+  @error('address')
+  <div id="inputAddressFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputPostcode">Poštanski broj</label>
-    <input type="number" class="form-control @if($errors->has('postcode')) is-invalid @endif" id="inputPostcode" name="postcode" value="{{ $client->postcode }}" aria-describedby="inputPostcodeFeedback">
-    
-    @if($errors->has('postcode'))
-    <div id="inputPostcodeFeedback" class="invalid-feedback">
-      {{ $errors->first('postcode') }}
-    </div>
-    @endif
+  @enderror
 
+</div>
+<div class="form-group">
+  <label for="inputPostcode">Poštanski broj</label>
+  <input type="number" class="form-control @error('postcode') is-invalid @enderror" id="inputPostcode" name="postcode" value="{{ $client->postcode }}" aria-describedby="inputPostcodeFeedback">
+  
+  @error('postcode')
+  <div id="inputPostcodeFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputCity">Grad</label>
-    <input type="text" class="form-control @if($errors->has('city')) is-invalid @endif" id="inputCity" name="city" value="{{ $client->city }}" aria-describedby="inputCityFeedback">
-    
-    @if($errors->has('city'))
-    <div id="inputCityFeedback" class="invalid-feedback">
-      {{ $errors->first('city') }}
-    </div>
-    @endif
+  @enderror
 
+</div>
+<div class="form-group">
+  <label for="inputCity">Grad</label>
+  <input type="text" class="form-control @error('city') is-invalid @enderror" id="inputCity" name="city" value="{{ $client->city }}" aria-describedby="inputCityFeedback">
+  
+  @error('city')
+  <div id="inputCityFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputCountry">Država</label>
-    <input type="text" class="form-control @if($errors->has('country')) is-invalid @endif" id="inputCountry" name="country" value="{{ $client->country }}" aria-describedby="inputCountryFeedback">
-    
-    @if($errors->has('country'))
-    <div id="inputCountryFeedback" class="invalid-feedback">
-      {{ $errors->first('country') }}
-    </div>
-    @endif
+  @enderror
 
+</div>
+<div class="form-group">
+  <label for="inputCountry">Država</label>
+  <input type="text" class="form-control @error('country') is-invalid @enderror" id="inputCountry" name="country" value="{{ $client->country }}" aria-describedby="inputCountryFeedback">
+  
+  @error('country'))
+  <div id="inputCountryFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <div class="form-group">
-    <label for="inputOIB">OIB</label>
-    <input type="number" class="form-control @if($errors->has('oib')) is-invalid @endif" id="inputOIB" name="oib" value="{{ $client->oib }}">
+  @enderror
 
-    @if($errors->has('oib'))
-    <div id="inputOIBFeedback" class="invalid-feedback">
-      {{ $errors->first('oib') }}
-    </div>
-    @endif
+</div>
+<div class="form-group">
+  <label for="inputOIB">OIB</label>
+  <input type="number" class="form-control @error('oib') is-invalid @enderror" id="inputOIB" name="oib" value="{{ $client->oib }}">
 
+  @error('oib')
+  <div id="inputOIBFeedback" class="invalid-feedback">
+    {{ $message }}
   </div>
-  <button type="submit" class="btn btn-primary">Spremi</button>
+  @enderror
+
+</div>
+<button type="submit" class="btn btn-primary">Spremi</button>
 </form>
 @endsection
