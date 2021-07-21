95b35d31fe63c45d8c473ee86ece754168f3768e
stipolovrenovic <74571935+stipolovrenovic@users.noreply.github.com>
2021-07-21 09:59:03 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,24 @@ require __DIR__.'/auth.php';
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
 
-Route::get('/clients', [ClientController::class, 'index'])->middleware(['auth'])->name('clients.index');
-Route::get('/clients/create', [ClientController::class, 'create'])->middleware(['auth'])->name('clients.create');
-Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
-Route::get('/clients/{client}', [ClientController::class, 'show'])->middleware(['auth'])->name('clients.show');
-Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->middleware(['auth'])->name('clients.edit');
-Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
-Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
 
-Route::get('/projects', [ProjectController::class, 'index'])->middleware(['auth'])->name('projects.index');
-Route::get('/projects/create', [ProjectController::class, 'create'])->middleware(['auth'])->name('projects.create');
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/projects/{project}', [ProjectController::class, 'show'])->middleware(['auth'])->name('projects.show');
-Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->middleware(['auth'])->name('projects.edit');
-Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+Route::middleware(['auth'])->group(function () {
+	Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
+	Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+	Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
+	Route::get('/clients/{client}', [ClientController::class, 'show'])->name('clients.show');
+	Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+	Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
+	Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
+
+	Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+	Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+	Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+	Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+	Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+	Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+	Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+});
+
+
+
